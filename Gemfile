6784c0b7e23065a4744d38024f425475decece10
Max Hellwig <max.hellwig@datenbetrieb.de>
2013-10-24 17:08:58 +0200

    Initial Commit This Commit contains all required things to get ready for templating

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+# Include Gems
+gem 'middleman'
+gem 'therubyracer'
+gem 'slim'
+gem 'susy'
+gem 'compass'
+gem 'middleman-livereload'
+gem 'oj'
+gem 'sprockets'
---
+++ config.rb
@@ -0,0 +1,49 @@
+#################################
+# General settings              #
+#################################
+
+# Requirements
+require 'compass'
+require 'susy'
+require 'gemfiles'
+
+# Asset locations
+set :css_dir 'Stylesheets'
+set :js_dir 'Javascripts'
+set :images_dir 'Images'
+set :font_dir 'Fonts'
+
+
+
+
+
+# Ignores
+
+######################################################################
+# Development environment.
+######################################################################
+
+configure :development do
+  # Indent html for pretty debugging
+  Slim::Engine.set_default_options pretty: true, sort_attrs: true
+  activate :livereload
+  # Enable Sass debug informations
+  compass_config do |compass|
+    compass.sass_options = {:debug_info => true}
+  end
+end
+
+######################################################################
+# Build environment.
+######################################################################
+
+configure :build do
+  activate :relative_assets
+  #activate :directory_indexes
+
+  # Ensmallen assets
+  #activate :minify_css
+  #activate :minify_javascript
+  #activate :gzip
+  #activate :asset_hash
+end
